7ce2211b2f763a1127799693eb7a6a388d3386b9
shohel rana <shohelranabadc@gmail.com>
2025-06-15 15:00:05 +0600

    window scroll properties practice pageYOffset and scrollY

---
+++ app.js
@@ -0,0 +1,27 @@
+// const pera = document.querySelector('.container p');
+
+// window.addEventListener('scroll', ()=> {
+//     console.clear();
+// console.log(window.scrollY);
+
+
+// // pageYOffset and scrollY both are same result but pageYOffset is old and all browsar compatible and scrollY is new and modern 
+
+// if (window.pageYOffset >= 400) {
+//     console.log('400 is over')
+//     pera.style.color = 'red'
+
+// } else if (window.scrollY >= 200) {
+//     pera.style.color = 'green'
+//     console.log('200 is over')
+// }else {
+//     pera.style.color = 'black'
+// }
+    
+
+// });
+
+console.log(window.innerHeight);
+console.log(window.outerHeight);
+console.log(window.innerWidth);
+console.log(window.outerWidth);
